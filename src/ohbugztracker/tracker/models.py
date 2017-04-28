39da09a405e43b512a5610529c3902762096d65b
Adnan Siddiqi <kadnan@gmail.com>
2017-04-28 17:33:57 +0500

    Part 3

--- src/ohbugztracker/tracker/models.py
+++ src/ohbugztracker/tracker/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    progress = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    severity = models.SmallIntegerField(default=0)
+    open_date = models.DateTimeField()
+    close_date = models.DateTimeField()
+    status = models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
--- src/ohbugztracker/tracker/views.py
+++ src/ohbugztracker/tracker/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project, Task
 
 
 def index(request):
-    return render(request, 'index.html')
+    projects = Project.objects.all()
+    return render(request, 'index.html', {'projects': projects})
